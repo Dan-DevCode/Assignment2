6fc385cb66fa96c880d303daa0b38bb1c81c7fc3
Dan-DevCode <162148967+Dan-DevCode@users.noreply.github.com>
2024-06-21 23:38:37 -0400

    Add files via upload

---
+++ controllers/productController.js
@@ -0,0 +1,60 @@
+const Product = require('../models/Product');
+
+exports.createProduct = async (req, res) => {
+  try {
+    const { name, description, price, quantity, category } = req.body;
+    const newProduct = new Product({ name, description, price, quantity, category });
+    await newProduct.save();
+    res.status(201).json(newProduct);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
+
+exports.getAllProducts = async (req, res) => {
+  try {
+    const products = await Product.find();
+    res.json(products);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
+
+exports.getProductById = async (req, res) => {
+  try {
+    const product = await Product.findById(req.params.id);
+    if (!product) {
+      return res.status(404).json({ message: 'Product not found' });
+    }
+    res.json(product);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
+
+exports.updateProduct = async (req, res) => {
+  try {
+    const { name, description, price, quantity, category } = req.body;
+    const updatedProduct = await Product.findByIdAndUpdate(req.params.id, 
+      { name, description, price, quantity, category }, 
+      { new: true });
+    if (!updatedProduct) {
+      return res.status(404).json({ message: 'Product not found' });
+    }
+    res.json(updatedProduct);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
+
+exports.deleteProduct = async (req, res) => {
+  try {
+    const deletedProduct = await Product.findByIdAndDelete(req.params.id);
+    if (!deletedProduct) {
+      return res.status(404).json({ message: 'Product not found' });
+    }
+    res.json({ message: 'Product deleted successfully' });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const productRoutes = require('./routes/productRoutes'); // Adjust path if necessary
+
+const app = express();
+
+app.use(express.json());
+
+const mongoURI = 'mongodb://localhost:27017/Marketplace'; 
+mongoose.connect(mongoURI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
+db.once('open', function () {
+  console.log('Connected to MongoDB');
+});
+
+app.use('/api', productRoutes); 
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
